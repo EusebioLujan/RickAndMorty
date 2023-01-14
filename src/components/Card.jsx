2a780866897ad23e64e47dc4c98a809ce403632a
EusebioLujan <narboleta@gmail.com>
2023-01-13 23:25:16 -0300

    RickandMorty fixcardid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,12 +11,17 @@ const Card = (props) => {
                 </button>
             </div>
             <Link to={`/detail/${props.id}`}><div className={styles.cardimage}>
-                <img className={styles.img} src={props.image} alt={props.name} />
+                <div className={styles.divimg}>
+                    
+                    <img className={styles.img} src={props.image} alt={props.name} />
+                    <p className={styles.ids}>ID:{props.id}</p>
+                </div>
                 <div className={styles.cardinfo}>
                         <p className={styles.cardname}>{props.name}</p>
                     <div className={styles.cardspeandgen}>
                         <p>Specie: {props.species}</p>
                         <p>Gender: {props.gender}</p>
+                        
                     </div>
                 </div>
             </div></Link>
